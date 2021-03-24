9ba9302a1d33b2dd3dc51f05a39fbcdeac5aea09
EyeWithItHandy <64708166+EyeWithItHandy@users.noreply.github.com>
2021-03-24 18:42:26 -0500

    visual sugerencias

--- lib/src/view/Sugerencias.dart
+++ lib/src/view/Sugerencias.dart
@@ -1,3 +1,4 @@
+import 'package:bouncing_widget/bouncing_widget.dart';
 import 'package:flutter/material.dart';
 
 class Sugerencias extends StatefulWidget {
@@ -8,7 +9,7 @@ class Sugerencias extends StatefulWidget {
 }
 
 class _SugerenciasState extends State<Sugerencias> {
- double height = 0;
+  double height = 0;
   double width = 0;
   @override
   Widget build(BuildContext context) {
@@ -30,7 +31,7 @@ class _SugerenciasState extends State<Sugerencias> {
                 child: Column(
                   children: [
                     Container(
-                      margin: EdgeInsets.only(top: height / 30 ),
+                      margin: EdgeInsets.only(top: height / 30),
                       child: Center(
                         child: Text(
                           'Sugerencias',
@@ -46,7 +47,9 @@ class _SugerenciasState extends State<Sugerencias> {
                       children: [
                         Container(
                           margin: EdgeInsets.only(
-                              top: height / 30, left: width / 20, right: width / 20),
+                              top: height / 30,
+                              left: width / 20,
+                              right: width / 20),
                           child: Text(
                             'Tu opinión es muy importante para nosotros y queremos hacer más grata tu experiencia con nostros dejanos tus dudas y nos comunicaremos contigo',
                             textAlign: TextAlign.justify,
@@ -57,39 +60,59 @@ class _SugerenciasState extends State<Sugerencias> {
                           ),
                         ),
                         Container(
+                          margin: EdgeInsets.only(top: height / 50),
                           child: Center(
                             child: Container(
                               decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(12)
-                              ),
+                                  borderRadius: BorderRadius.circular(12)),
                               height: height / 5,
                               width: width / 1.3,
                               child: Center(
                                 child: TextField(
                                   maxLines: 5,
                                   decoration: InputDecoration(
-                                    labelText: 'Cuentanos en que podemos mejorar',
+                                    labelText:
+                                        'Cuentanos en que podemos mejorar',
                                     focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color:  Color.fromRGBO(101, 79, 168, 1),
-                                        width: 1
-                                      )
-                                    ),
-                                    enabledBorder:OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color:  Color.fromRGBO(101, 79, 168, 1),
-                                        width: 1
-                                      )
-                                    ), 
+                                        borderSide: BorderSide(
+                                            color:
+                                                Color.fromRGBO(101, 79, 168, 1),
+                                            width: 1)),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color:
+                                                Color.fromRGBO(101, 79, 168, 1),
+                                            width: 1)),
                                   ),
                                 ),
                               ),
                             ),
                           ),
                         ),
-
-                        
-                       
+                        Container(
+                          child: BouncingWidget(
+                              duration: Duration(milliseconds: 100),
+                              scaleFactor: 1.5,
+                              onPressed: () {},
+                              child: Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50.0),
+                                ),
+                                color: Color.fromRGBO(101, 79, 168, 1),
+                                child: Container(
+                                  width: width / 3,
+                                  height: height / 20,
+                                  child: Text(
+                                    "Enviar",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(255, 255, 255, 1),
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 30),
+                                  ),
+                                ),
+                              )),
+                        ),
                       ],
                     ),
                   ],
