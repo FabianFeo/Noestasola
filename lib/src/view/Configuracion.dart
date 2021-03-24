a22478605234f2db841ed713063eb0bfef86fd9b
EyeWithItHandy <64708166+EyeWithItHandy@users.noreply.github.com>
2021-03-23 20:20:06 -0500

    visual configuracion

--- lib/src/view/Configuracion.dart
+++ lib/src/view/Configuracion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 class Config extends StatefulWidget {
   Config({Key key}) : super(key: key);
@@ -35,10 +36,39 @@ class _ConfigState extends State<Config> {
                 )),
               ),
               Container(
-                margin: EdgeInsets.only(top: height / 2),
-                child: Center(
-                  child: Text('data'),
-                ),
+                margin: EdgeInsets.only(top: height / 20),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(
+                        'SMS',
+                        style: TextStyle(
+                          color:Color.fromRGBO(101, 79, 168, 1),
+                          fontWeight: FontWeight.w500,
+                          fontSize: height / 50, 
+
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: height / 30,
+                     width: width / 5,
+                      child: LiteRollingSwitch(  
+                        animationDuration: Duration(milliseconds: 2),                      
+                        value: true,
+                        textOn: 'On',
+                        textOff: 'off',
+                        colorOn: Color.fromRGBO(101, 79, 168, 1),
+                        colorOff: Colors.grey,
+                        iconOn: Icons.auto_fix_high,
+                        iconOff: Icons.auto_fix_off,
+                        onChanged: (bool position) {
+                          print('el boton es $position');
+                        },
+                      ),
+                    )
+                  ],
+                )
               )
                  ],
                ),
